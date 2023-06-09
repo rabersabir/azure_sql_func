1d6a0fb641b8c73e25104e5322ae5d88c15cce22
Raber Sabir <rabersabir@hotmail.com>
2023-06-09 16:59:29 +0200

    init commit

---
+++ EmptyClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Azure.WebJobs.Extensions.Sql;
+
+public class Todo
+{
+
+    public Guid id { get; set};
+}
+
+namespace AzureSQL.ToDo
+{
+    public static class ToDoTrigger
+    {
+        [FunctionName("ToDoTrigger")]
+        public static void Run(
+            [SqlTrigger("[dbo].[ToDo]", "SqlConnectionString")]
+            IReadOnlyList<SqlChange<Todo>> changes,
+            ILogger logger)
+        {
+            foreach (SqlChange<ToDoItem> change in changes)
+            {
+                ToDoItem toDoItem = change.Item;
+                logger.LogInformation($"Change operation: {change.Operation}");
+                logger.LogInformation($"Id: {toDoItem.Id}, Title: {toDoItem.title}, Url: {toDoItem.url}, Completed: {toDoItem.completed}");
+            }
+        }
+    }
+}
